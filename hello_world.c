7c4140c5cf0c56b86fd27dbdc04180b49877f224
PerviousPie8 <majed.alghamdi-4@student.manchester.ac.uk>
2025-02-13 13:08:29 +0000

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void){
 
-    printf("Hello. My email address is majed.alghamdi-4@student.manchester.ac.uk");
+    printf("Hello. My email address is:\nmajed.alghamdi-4@student.manchester.ac.uk");
 
     return 0;
 
